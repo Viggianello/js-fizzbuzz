96b2d3fd36c585e03b36ba1039fadfa539c9fbed
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-22 14:13:22 +0200

    stampo solo i numeri da 0 a 100 e quelli divisibili per tre stampo solo Fizz e non il numero

--- main.js
+++ main.js
@@ -3,7 +3,10 @@
 //     console.log (num);
 // }
 // programma che stampa i primi numeri da 0 a 100 ma per i multipli di 3 stampi al suo posto Fizz
-for (var num = 0; num < 101 ; num++) {
+// var verifica= parseInt(0);
+// var v= verifica;
+console.log (0 + ' dato che sono un informatico inizio a contare da 0');
+for (var num = 1; num < 101 ; num++) {
 
     if (num % 3 != 0) {
         console.log (num);
