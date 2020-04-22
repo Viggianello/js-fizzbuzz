02feb88507c2f9e4ddb8874af4db2640ba6e9e35
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-04-22 14:21:04 +0200

    fixed 0 con zero

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 // programma che stampa i primi numeri da 0 a 100 ma per i multipli di 3 stampi al suo posto Fizz
 // var verifica= parseInt(0);
 // var v= verifica;
-console.log (0 + ' dato che sono un informatico inizio a contare da 0');
+console.log (0 + ' dato che sono un informatico inizio a contare da zero');
 for (var num = 1; num < 101 ; num++) {
 
     if (num % 3 == 0 && num % 15 !=0) {
